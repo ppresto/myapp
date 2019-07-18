97f9187771e07940de792bb70d438655f3dae3a3
ppresto <pgpresto@gmail.com>
2019-07-17 21:47:00 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 // Modules
 module "instance_module" {
   source  = "app.terraform.io/ppresto_ptfe/instance-module/aws"
-  version = "1.0.3"
+  version = "1.0.4"
 
   egress_cidr_block = "0.0.0.0/0"
   ingress_cidr_block = "157.131.174.226/32"
