2ee67db0f952cd3318a955d5fe54d694b1eb1e15
ppresto <pgpresto@gmail.com>
2019-07-17 18:21:43 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,9 @@
 // Modules
 module "instance_module" {
   source  = "app.terraform.io/ppresto_ptfe/instance-module/aws"
-  version = "1.0.6-k-cidr"
+  version = "1.0.3"
 
-  egress_cidr_block = "0.0.0.0/0"
-  ingress_cidr_block = "157.131.174.226/32"
-  instance_type = "t2.micro"
   namespace = "ppresto"
-  public = "true"
-  region = "us-west-2"
 }
 
 output "IP Address" {
