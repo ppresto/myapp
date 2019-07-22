e87a4ad1b456095724ec1bfcdd7e35cb8ce607a2
ppresto <pgpresto@gmail.com>
2019-07-22 12:07:57 -0700

    Create aws_instance.tf

---
+++ aws_instance.tf
@@ -0,0 +1,27 @@
+//--------------------------------------------------------------------
+// Modules
+module "instance_module" {
+  source  = "app.terraform.io/ppresto_ptfe/instance-module/aws"
+  version = "1.0"
+
+  egress_cidr_block = "0.0.0.0/0"
+  ingress_cidr_block = "157.131.174.226/32"
+  instance_type = "t2.micro"
+  namespace = "ppresto"
+}
+
+output "ip_address" {
+    value = "${module.instance_module.public_ip}"
+}
+  
+output "private_key" {
+    value = "${module.instance_module.private_key_pem}"
+}
+
+output "public_key" {
+    value = "${module.instance_module.public_key_pem}"
+}
+
+output "aws_keypair_name" {
+    value = "${module.instance_module.aws_keypair_name}"
+}
