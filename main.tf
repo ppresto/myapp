5614c004c1e42ddcbc7ab03d90055f367b228097
ppresto <ppresto@hashicorp.com>
2019-07-24 15:46:59 -0700

    new gcp module

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 //--------------------------------------------------------------------
 // Modules
 module "compute_instance" {
-  source  = "app.terraform.io/ppresto_ptfe/compute-instance/google"
+  source  = "app.terraform.io/Patrick/compute-instance/google"
   version = "0.1.4"
-
-  count = 1
 }
