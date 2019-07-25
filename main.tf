059426b0e772d2b522ae0b17d465a8b2c20efb66
ppresto <ppresto@hashicorp.com>
2019-07-25 16:50:06 -0700

    latest ops env

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 //--------------------------------------------------------------------
 // Modules
-module "compute_instance" {
-  source  = "app.terraform.io/Patrick/compute-instance/google"
-  version = "0.1.4"
+module "ecs_fargate" {
+  source  = "app.terraform.io/Patrick/ecs-fargate/aws"
+  version = "0.4"
+
+  alb_ingress_cidrblock = "57.131.174.226/32"
+  app_count             = 1
+  app_port              = 8080
+  name_prefix           = "ppresto-test1"
 }
