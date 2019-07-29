164666d6275eb94a543de771267c7fbf39bf8c9f
ppresto <ppresto@hashicorp.com>
2019-07-29 12:14:12 -0700

    test2

--- aws_instance.tf
+++ aws_instance.tf
@@ -2,7 +2,7 @@
 // Modules
 module "instance" {
   source  = "app.terraform.io/Patrick/aws_instance/aws"
-  version = "1.4"
+  version = "1.5"
 
   count              = 1
   ingress_cidr_block = "157.131.174.226/32"
