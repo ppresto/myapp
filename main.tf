2817c0e2b2544be3d676d58570e3d091c08c82c7
ppresto <ppresto@hashicorp.com>
2019-07-29 12:47:27 -0700

    test2

--- main.tf
+++ main.tf
@@ -6,4 +6,20 @@ module "aws_instance" {
 
   ingress_cidr_block = "157.131.174.226/32"
   name_prefix        = "ppresto-t1"
+
+  output "ip_address" {
+    value = "${module.instance_module.public_ip}"
+  }
+
+  output "private_key" {
+    value = "${module.instance_module.private_key_pem}"
+  }
+
+  output "public_key" {
+    value = "${module.instance_module.public_key_pem}"
+  }
+
+  output "aws_keypair_name" {
+    value = "${module.instance_module.aws_keypair_name}"
+  }
 }
