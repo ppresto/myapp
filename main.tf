a69463e8ffcc060ae7a38b02a5a6d8b7c627ee86
ppresto <ppresto@hashicorp.com>
2019-07-29 12:24:42 -0700

    test4

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "azure_instance" {
   source  = "app.terraform.io/Patrick/azure_instance/azure"
   version = "1.0"
 
-  count_linux   = 1
+  count_linux   = 0
   count_windows = 1
   dns_prefix    = "ppresto-myazurewinhost"
 }
