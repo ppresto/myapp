d2ddea3c3a3165e7c945cafd99a5db916d06c077
ppresto <ppresto@hashicorp.com>
2019-07-29 14:12:48 -0700

    ecs

--- main.tf
+++ main.tf
@@ -4,8 +4,10 @@ module "ecs_fargate" {
   source  = "app.terraform.io/Patrick/ecs-fargate/aws"
   version = "0.4"
 
-  alb_ingress_cidrblock = "157.131.174.226/32"
+  alb_ingress_cidrblock = "57.131.174.226/32"
   app_count             = 2
+  app_image             = "nginxdemos/hello"
+  app_port              = 8080
   name_prefix           = "pp-myapp-ecs"
 }
 
