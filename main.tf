03397934cf170927d16e134724cb77d5cac7f727
ppresto <ppresto@hashicorp.com>
2019-07-29 11:25:53 -0700

    my azure env

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 //--------------------------------------------------------------------
 // Modules
-module "ecs_fargate" {
-  source  = "app.terraform.io/Patrick/ecs-fargate/aws"
-  version = "0.4"
+module "instance" {
+  source  = "app.terraform.io/Patrick/instance/azure"
+  version = "1.0"
 
-  alb_ingress_cidrblock = "57.131.174.226/32"
-  app_count             = 1
-  app_port              = 8080
-  name_prefix           = "ppresto-test1"
+  count_linux = 0
+  count_windows = 1
+  dns_prefix = "patrickwindowshost"
 }
