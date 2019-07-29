f6813d7681bd38e5ae0efc14fab8f13047ea4fe1
ppresto <ppresto@hashicorp.com>
2019-07-29 14:19:01 -0700

    test ecs

--- main.tf
+++ main.tf
@@ -1,16 +1,27 @@
 //--------------------------------------------------------------------
 // Modules
-module "ecs_fargate" {
-  source  = "app.terraform.io/Patrick/ecs-fargate/aws"
-  version = "0.4"
+module "aws_instance" {
+  source  = "app.terraform.io/Patrick/aws_instance/aws"
+  version = "1.5"
 
-  alb_ingress_cidrblock = "57.131.174.226/32"
-  app_count             = 2
-  app_image             = "nginxdemos/hello"
-  app_port              = 8080
-  name_prefix           = "pp-myapp-ecs"
+  count              = 1
+  instance_type      = "t2.small"
+  name_prefix        = "myapp-pp"
+  ingress_cidr_block = "157.131.174.226/32"
 }
 
 output "ip_address" {
-  value = "${module.ecs_fargate.alb_hostname}"
+  value = "${module.aws_instance.public_ip}"
+}
+
+output "private_key" {
+  value = "${module.aws_instance.private_key_pem}"
+}
+
+output "public_key" {
+  value = "${module.aws_instance.public_key_pem}"
+}
+
+output "aws_keypair_name" {
+  value = "${module.aws_instance.aws_keypair_name}"
 }
