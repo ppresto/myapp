1d57ddaab1b372f461d8abd9aca0bc98e57703bf
ppresto <ppresto@hashicorp.com>
2019-09-05 16:51:16 -0700

    outputs

--- main.tf
+++ main.tf
@@ -18,3 +18,19 @@ module "ec2_instance" {
   vpc_id = "${data.terraform_remote_state.patrick_tf_aws_standard_network.vpc_id}"
   count = 1
 }
+
+output "private_key_filename" {
+  value = "${module.ec2_instance.private_key_filename}"
+}
+
+output "private_key_pem" {
+  value = "${module.ec2_instance.private_key_pem}"
+}
+
+output "my_nodes_public_ips" {
+  value = "${module.ec2_instance.public_ip}"
+}
+
+output "my_bastion_public_ips" {
+  value = "${module.ec2_instance.bastion_ips_public}"
+}
