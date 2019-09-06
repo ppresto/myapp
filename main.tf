271dcdf920da6cf6ed5409314192901bdecff182
ppresto <ppresto@hashicorp.com>
2019-09-05 21:47:03 -0700

    remove name

--- main.tf
+++ main.tf
@@ -10,10 +10,9 @@ module "ec2_instance" {
   source  = "app.terraform.io/Patrick/ec2_instance/aws"
   version = "0.1.5"
   
-  name_prefix = "ppresto2-dev"
-  subnet_id = "${data.terraform_remote_state.patrick_tf_aws_standard_network.subnet_private_ids[0]}"
   count = 1
   instance_type = "t2.micro"
+  subnet_id = "${data.terraform_remote_state.patrick_tf_aws_standard_network.subnet_private_ids[0]}"
 }
 
 //--------------------------------------------------------------------
