ff2abd86f6a64f0dba16f936d1e411f06d30fec6
ppresto <ppresto@hashicorp.com>
2019-09-05 23:13:33 -0700

    remove old mod

--- main.tf
+++ main.tf
@@ -1,24 +1,45 @@
 //--------------------------------------------------------------------
-// Modules
+// Module - GCP Instances
+module "gce_instance" {
+  source  = "app.terraform.io/Patrick/gce_instance/google"
+  version = "0.1.4"
+}
+//--------------------------------------------------------------------
+// Module - AWS Instances
 module "ec2_instance" {
   source  = "app.terraform.io/Patrick/ec2_instance/aws"
-  version            = "0.1.0"
-  name_prefix        = "${var.name_prefix}"
-  instance_type      = "${var.instance_type}"
-  ingress_cidr_block = "${var.ingress_cidr_block}"
+  version = "0.1.5"
+  name_prefix = "${var.name_prefix}"
+  count = 1
+  instance_type = "t2.micro"
+  subnet_id = "${data.terraform_remote_state.patrick_tf_aws_standard_network.subnet_private_ids[0]}"
 }
 
+//--------------------------------------------------------------------
+// AWS Network - Get Network Team's Existing VPC Data from their Workspace
+data "terraform_remote_state" "patrick_tf_aws_standard_network" {
+  backend = "atlas"
+  config {
+    address = "https://app.terraform.io"
+    name    = "Patrick/tf-aws-standard-network"
+  }
+}
+
+//--------------------------------------------------------------------
+// OUTPUTS - For Useability
 output "private_key_filename" {
   value = "${module.ec2_instance.private_key_filename}"
 }
 output "private_key_pem" {
   value = "${module.ec2_instance.private_key_pem}"
 }
-output "AWS_Address" {
-  value = "${module.ec2_instance.public_ip}"
+output "my_nodes_public_ips" {
+  value = "${module.ec2_instance.my_nodes_public_ips}"
 }
-
-output "aws_keypair_name" {
-  value = "${module.ec2_instance.aws_keypair_name}"
+output "my_bastion_public_ips" {
+  value = "${module.ec2_instance.my_bastion_public_ips}"
+}
+output "GCP_Address" {
+  value = "${module.gce_instance.addresses}"
 }
 
