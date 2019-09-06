7e92b8f6535f6599840a44471cb1d31fc7cbd6b6
ppresto <ppresto@hashicorp.com>
2019-09-05 17:02:19 -0700

    subnet id test

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ data "terraform_remote_state" "patrick_tf_aws_standard_network" {
 // Modules
 module "ec2_instance" {
   source  = "app.terraform.io/Patrick/ec2_instance/aws"
-  version = "0.1.4"
+  version = "0.1.5"
   
   name_prefix = "ppresto2-dev"
-  vpc_id = "${data.terraform_remote_state.patrick_tf_aws_standard_network.vpc_id}"
+  subnet_id = "${data.terraform_remote_state.patrick_tf_aws_standard_network.subnet_private_ids[0]}"
   count = 1
 }
 
